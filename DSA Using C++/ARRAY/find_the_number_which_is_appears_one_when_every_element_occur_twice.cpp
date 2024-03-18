437a48b27da53569315edef8d0f541b98406025e
Harsh Chauhan <118177237+HarshChauhan05@users.noreply.github.com>
2024-03-19 01:03:40 +0530

    Update find_the_number_which_is_appears_one_when_every_element_occur_twice.cpp

--- DSA Using C++/ARRAY/find_the_number_which_is_appears_one_when_every_element_occur_twice.cpp
+++ DSA Using C++/ARRAY/find_the_number_which_is_appears_one_when_every_element_occur_twice.cpp
@@ -1,1 +1,13 @@
+int getSingleElement(vector<int> &arr) {
+    int n = arr.size();
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {  // xor of two same number  is zero. A^A =0 || A^0=A
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
 
+
+this can be solve by Hashing. 
+try yourself. 
