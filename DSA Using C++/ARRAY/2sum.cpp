6f4360ad62f0e70edd54a1fa125257f6e806db61
Harsh Chauhan <118177237+HarshChauhan05@users.noreply.github.com>
2024-03-20 01:04:22 +0530

    Create 2sum.cpp

---
+++ DSA Using C++/ARRAY/2sum.cpp
@@ -0,0 +1,8 @@
+string twoSum(int n, vector<int> &arr, int target) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) return "YES";
+        }
+    }
+    return "NO";
+}
