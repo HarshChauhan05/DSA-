07adda7ef85d2a7c084b3ae4f2ad3c9d26ad310f
Harsh Chauhan <118177237+HarshChauhan05@users.noreply.github.com>
2024-03-19 14:07:34 +0530

    Create Longest_subarray_of_sum_k.cpp

---
+++ DSA Using C++/ARRAY/Longest_subarray_of_sum_k.cpp
@@ -0,0 +1,17 @@
+int LongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        long long s = 0; // Sum variable
+        for (int j = i; j < n; j++) { // ending index
+            // add the current element to
+            // the subarray a[i...j-1]:
+            s += a[j];
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
