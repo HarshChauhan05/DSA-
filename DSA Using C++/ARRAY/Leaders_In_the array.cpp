e66787e95812a84cddd68e2a83cc7b4d66e2d118
Harsh Chauhan <118177237+HarshChauhan05@users.noreply.github.com>
2024-03-21 01:11:55 +0530

    Create Leaders_In_the array.cpp

---
+++ DSA Using C++/ARRAY/Leaders_In_the array.cpp
@@ -0,0 +1,26 @@
+vector<int> printLeadersBruteForce(int arr[], int n) {
+
+  vector<int> ans;
+  
+  for (int i = 0; i < n; i++) {
+    bool leader = true;
+
+    //Checking whether arr[i] is greater than all 
+    //the elements in its right side
+    for (int j = i + 1; j < n; j++)
+      if (arr[j] > arr[i]) {
+          
+        // If any element found is greater than current leader
+        // curr element is not the leader.
+        leader = false;
+        break;
+      }
+
+    // Push all the leaders in ans array.
+    if (leader)
+    ans.push_back(arr[i]);
+
+  }
+  
+  return ans;
+}
