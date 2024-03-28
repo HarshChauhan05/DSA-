10c1fa08ed48cd2fbafe7a561f9aa2d0f052b650
Harsh Chauhan <118177237+HarshChauhan05@users.noreply.github.com>
2024-03-28 11:23:53 +0530

    Update count_sort.cpp

--- DSA Using C++/ARRAY/String/count_sort.cpp
+++ DSA Using C++/ARRAY/String/count_sort.cpp
@@ -1,1 +1,17 @@
+String cout_sort(string str){
+  // need to store the frequency
+  vector<int> freq(26,0);
+  for(int i=0;i<str.length;i++){
+    int index =str[i]-'a';
+    freq[index]++;
+  }
 
+  // sort the string
+  int j=0;
+  for(int i=0;i<26;i++){
+    while(str[i]--){
+      str[j++]=i+'a';
+    }
+  }
+  return str;
+}
