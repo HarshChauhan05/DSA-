6653e56ec53e17349f20c08bb146151e92ed789a
Pankaj Gangwar <129405105+pankajgangwar9@users.noreply.github.com>
2024-04-02 23:21:44 +0530

    Leader in an Array (Cod

---
+++ DSA Using Java/Arrays Question/LeaderInAArray.java
@@ -0,0 +1,15 @@
+public class LeaderElement{
+
+public static void main(String[] args){
+
+int arr = {15,16,3,2,6,1,4};
+int max = arr[n-1];
+System.out.println(arr[n-1]);
+for(int i=n-1;i>=0;i--){
+   if(arr[i]>max){
+     System.out.println(arr[i]);
+     max=arr[i];
+ }
+  }
+ }
+}
